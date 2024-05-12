4e21b578121f524625e0c31d96eb02c78e6fa934
Jules VINET LATRILLE <julesvinet64@gmail.com>
2024-05-12 13:47:06 +0200

    Retour de la version test

---
+++ v2MVP_test/code/lecteurvue.h
@@ -0,0 +1,37 @@
+#ifndef LECTEURVUE_H
+#define LECTEURVUE_H
+
+#include <QMainWindow>
+#include <QDebug>
+#include <QShortcut>
+#include <QWidget>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class lecteurVue; }
+QT_END_NAMESPACE
+
+class lecteurVue : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    lecteurVue(QWidget *parent = nullptr);
+    ~lecteurVue();
+
+public slots:
+    void sl_suivant();
+    void sl_precedent();
+    void sl_pause();
+    void sl_chargerDiapo();
+    void sl_quitter();
+    void sl_enleverDiapo();
+    void sl_vitesseDefilement();
+    void sl_modeAuto();
+    void sl_modeManuel();
+    void sl_aPropos();
+
+private:
+    Ui::lecteurVue *ui;
+
+};
+#endif // LECTEURVUE_H
