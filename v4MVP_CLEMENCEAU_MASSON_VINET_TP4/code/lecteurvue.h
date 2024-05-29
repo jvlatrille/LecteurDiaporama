3b66635ac9fbd3345b7fa3f006665c2959a3856d
Rafael Masson <rmasson003@iutbayonne.univ-pau.fr>
2024-05-29 09:14:43 +0200

    fix erreur v4mvp sur slot departarretauto

--- v4MVP_CLEMENCEAU_MASSON_VINET_TP4/code/lecteurvue.h
+++ v4MVP_CLEMENCEAU_MASSON_VINET_TP4/code/lecteurvue.h
@@ -32,7 +32,6 @@ public:
 public slots:
     void demanderAvancer();
     void demanderReculer();
-    void demanderDepartArretAuto();
     void demanderChangerVitesse();
     void demanderChangerModeAutomatique();
     void demanderChangerModeManuel();
